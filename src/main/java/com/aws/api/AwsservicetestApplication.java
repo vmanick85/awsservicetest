5fd7ec911d49b5691b775b64abd0166b579ce9af
Vinoth <vinoth.m.ettiappan@gmail.com>
2017-04-28 08:00:40 -0500

    setting up the project

---
+++ src/main/java/com/aws/api/AwsservicetestApplication.java
@@ -0,0 +1,12 @@
+package com.aws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsservicetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsservicetestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/aws/api/controller/AWServiceController.java
@@ -0,0 +1,8 @@
+package com.aws.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AWServiceController {
+
+}
