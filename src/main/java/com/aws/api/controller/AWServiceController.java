d76d676cca6442fd821f1b54102237c46fc90ec2
Vinoth <vinoth.m.ettiappan@gmail.com>
2017-04-29 17:23:03 -0500

    Added controller and modified application.properties/gradle

--- src/main/java/com/aws/api/controller/AWServiceController.java
+++ src/main/java/com/aws/api/controller/AWServiceController.java
@@ -1,8 +1,20 @@
 package com.aws.api.controller;
 
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AWServiceController {
+	
+	@RequestMapping(value="order/list",method=RequestMethod.GET)
+	@ResponseBody
+	public List<String>  getOrders(){
+        return Lists.newArrayList("order","product");
+	}
 
 }
